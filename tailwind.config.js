07dabd9ea3be5d5b6d1db6e71350abc4bed9ca67
«Sviatoslav» <ksvyat21@gmail.com>
2024-05-29 19:25:13 +0200

    feat(tailwind): extend tailwind with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,11 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'custom-gradient': 'linear-gradient(0deg, rgba(45,194,223,1) 0%, rgba(0,80,255,1) 100%)',
       },
+      textColor: {
+        "custom-color": "linear-gradient(0deg, rgba(6,79,228,1) 0%, rgba(3,166,242,1) 48%, rgba(0,243,255,1) 100%)"
+      }
     },
   },
   plugins: [],
